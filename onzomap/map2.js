270952a8a0599bf0426dfa3e0c9873a58fc0cee7
36wish <felix.lee@sparkventures.co.nz>
2017-11-06 10:05:42 +1300

    change cors proxy to yahoo

--- onzomap/map2.js
+++ onzomap/map2.js
@@ -33,31 +33,40 @@ function initMap() {
 		content: '1234',
 		pixelOffset: new google.maps.Size(-8, 10)
 	});
+// https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20json%20where%20url%3D%22https%3A%2F%2Fapp.onzo.co.nz%2Fnearby%2F-36.848123%2F174.765588%2F50.0%22&format=json&diagnostics=true&callback=
+	// $.getJSON(`https://cors-anywhere.herokuapp.com/https://app.onzo.co.nz/nearby/${auckland.lat}/${auckland.lng}/50.0`, function(json) {
+	$.getJSON("http://query.yahooapis.com/v1/public/yql",
+		{
+			q: `select * from json where url="https://app.onzo.co.nz/nearby/${auckland.lat}/${auckland.lng}/50.0"`,
+			format: "json"
+		},
+		function(json){	
+			$('#overlay').remove()
+			console.log(json)
+			console.log(json.query.results.json.data)
+			json.query.results.json.data.forEach(function(e){
+				console.log(e)
+				console.log(e.latitude, e.longitude)
+				var marker = new google.maps.Marker({
+					position: {lat: parseFloat(e.latitude), lng: parseFloat(e.longitude)},
+					//label: e.id + '',
+					map: map,
+					// icon: e.isLock == 0 ? pinImage2 : pinImage
+					icon: pinImage
+				});
+				var date = new Date(parseInt(e.createTime))
+				marker.addListener('click', function() {
+					infowindow.setContent(
+						`<div><b>${e.producid}</b></div>
+						<div>Unlocked times: ${e.unlockedTimes}</div>	
+						<div>isLock: ${e.isLock}</div>
+						<div>Created: ${date.getDate()}/${date.getMonth()+1}/${date.getFullYear()}</div>
 
-	$.getJSON(`https://cors-anywhere.herokuapp.com/https://app.onzo.co.nz/nearby/${auckland.lat}/${auckland.lng}/50.0`, function(json) {
-		$('#overlay').remove()
-		console.log(json.data)
-		json.data.forEach(function(e){
-			var marker = new google.maps.Marker({
-				position: {lat: e.latitude, lng: e.longitude},
-				//label: e.id + '',
-				map: map,
-				// icon: e.isLock == 0 ? pinImage2 : pinImage
-				icon: pinImage
-			});
-			var date = new Date(e.createTime)
-			marker.addListener('click', function() {
-				infowindow.setContent(
-					`<div><b>${e.producid}</b></div>
-					<div>Unlocked times: ${e.unlockedTimes}</div>	
-					<div>isLock: ${e.isLock}</div>
-					<div>Created: ${date.getDate()}/${date.getMonth()+1}/${date.getFullYear()}</div>
-
-													`)
-				infowindow.open(map, marker);
-			});
-			pins.push(marker)
-		})
-		console.log(pins)
-	});
+														`)
+					infowindow.open(map, marker);
+				});
+				pins.push(marker)
+			})
+			console.log(pins)
+		});
 }
