649141e1f54a2eef82a8a80749465f6ec8809bd4
36wish <felix.lee@sparkventures.co.nz>
2017-11-06 13:06:00 +1300

    auto refresh 5 min

--- onzomap/map2.js
+++ onzomap/map2.js
@@ -1,3 +1,9 @@
+var map, pinImage, pinImage2, infowindow;
+var pins = [];
+const aucklandLat = -36.848123;
+const aucklandLng = 174.765588;
+const pageTitle = document.title;
+
 function updateHash(map){
 	const rnd = 6
 	console.log('zoom',map.getZoom())
@@ -8,8 +14,7 @@ function updateHash(map){
 
 function initMap() {
 	// var auckland = {lat: -36.848123, lng: 174.765588};
-	const aucklandLat = -36.848123;
-	const aucklandLng = 174.765588;
+
 	var lat, lng, zoom;
 	
 	if (/#-\d+.?\d*\/\d+.?\d*\/\d{1,2}/.test(window.location.hash)){
@@ -23,15 +28,15 @@ function initMap() {
 		zoom = parseInt(localStorage.getItem('zoom')) || 16
 	}
 	console.log(lat, lng, zoom)
-	var map = new google.maps.Map(document.getElementById('map'), {
+	map = new google.maps.Map(document.getElementById('map'), {
 		zoom: zoom,
 		center: {lat:lat, lng:lng}
 	});
 
 	map.addListener('idle',function(){
-		console.log('zoom',map.getZoom())
-		console.log('lat',map.getCenter().lat())
-		console.log('lng',map.getCenter().lng())
+		// console.log('zoom',map.getZoom())
+		// console.log('lat',map.getCenter().lat())
+		// console.log('lng',map.getCenter().lng())
 		localStorage.setItem('zoom', map.getZoom())
 		localStorage.setItem('lat', map.getCenter().lat())
 		localStorage.setItem('lng', map.getCenter().lng())
@@ -47,17 +52,17 @@ function initMap() {
 	// }
 
 	var bikeLayer = new google.maps.BicyclingLayer();
-    bikeLayer.setMap(map);
+	bikeLayer.setMap(map);
 
-    var pinImage = new google.maps.MarkerImage("http://chart.apis.google.com/chart?chst=d_map_pin_letter_withshadow&chld=%E2%80%A2|ffeb3b",
-        new google.maps.Size(40,37),
-        new google.maps.Point(0,0),
-        new google.maps.Point(11, 34));
+	pinImage = new google.maps.MarkerImage("http://chart.apis.google.com/chart?chst=d_map_pin_letter_withshadow&chld=%E2%80%A2|ffeb3b",
+		new google.maps.Size(40,37),
+		new google.maps.Point(0,0),
+		new google.maps.Point(11, 34));
 
-    var pinImage2 = new google.maps.MarkerImage("http://chart.apis.google.com/chart?chst=d_map_pin_letter_withshadow&chld=%E2%80%A2|2765e0",
-        new google.maps.Size(40,37),
-        new google.maps.Point(0,0),
-        new google.maps.Point(11, 34));
+	pinImage2 = new google.maps.MarkerImage("http://chart.apis.google.com/chart?chst=d_map_pin_letter_withshadow&chld=%E2%80%A2|2765e0",
+		new google.maps.Size(40,37),
+		new google.maps.Point(0,0),
+		new google.maps.Point(11, 34));
 	// var infowindow = new google.maps.InfoWindow({
  //          content: 'Change the zoom level',
  //          position: auckland
@@ -68,20 +73,29 @@ function initMap() {
  //          infowindow.setContent('Zoom: ' + map.getZoom());
  //        });
 
- 	var pins = []
+	
 
-	var infowindow = new google.maps.InfoWindow({
+	infowindow = new google.maps.InfoWindow({
 		content: '1234',
 		pixelOffset: new google.maps.Size(-8, 10)
 	});
 // https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20json%20where%20url%3D%22https%3A%2F%2Fapp.onzo.co.nz%2Fnearby%2F-36.848123%2F174.765588%2F50.0%22&format=json&diagnostics=true&callback=
 	// $.getJSON(`https://cors-anywhere.herokuapp.com/https://app.onzo.co.nz/nearby/${auckland.lat}/${auckland.lng}/50.0`, function(json) {
+	
+	// setInterval()
+	updatePins();
+	setInterval(updatePins,1000*60*5);
+}
+
+function updatePins(){
 	$.getJSON("https://query.yahooapis.com/v1/public/yql",
 		{
 			q: `select * from json where url="https://app.onzo.co.nz/nearby/${aucklandLat}/${aucklandLng}/50.0"`,
 			format: "json"
 		},
-		function(json){	
+		function(json){
+			pins.forEach(e=>e.setMap(null))
+			pins = [];	
 			$('#overlay').remove()
 			console.log(json)
 			console.log(json.query.results.json.data)
@@ -108,6 +122,6 @@ function initMap() {
 				});
 				pins.push(marker)
 			})
-			console.log(pins)
+			//console.log(pins)
 		});
 }
