d2515e2353780dd8f55406d2808ea2bfcd6330b1
36wish <felix.lee@sparkventures.co.nz>
2018-07-05 15:18:04 +1200

    script

--- _includes/script.js
+++
@@ -1,5 +0,0 @@
-console.log('123')
-codes = ['ABCDEF','BBBBBB','CCCCCC']
-$() {
-	$('strong:contains("SAQIZJ")').text(codes[Math.floor(Math.random()*codes.length)])
-}
--- assets/script/script.js
+++ assets/script/script.js
@@ -1,2 +1,5 @@
 console.log('123')
-$()
+codes = ['ABCDEF','BBBBBB','CCCCCC']
+$() {
+	$('strong:contains("SAQIZJ")').text(codes[Math.floor(Math.random()*codes.length)])
+}
